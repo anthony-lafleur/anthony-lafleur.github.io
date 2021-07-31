4865187b980581f6aec1f9fa95a6a84f066ec395
anthony-lafleur <58486696+anthony-lafleur@users.noreply.github.com>
2021-07-31 19:23:48 -0400

    ROARROC update

--- ROARROC/menu.js
+++ ROARROC/menu.js
@@ -7,10 +7,12 @@ function openMenu() {
     if (menu === 0) {
         console.log("opening menu");
         document.getElementById("nav").classList.add('nav_open');
+        document.getElementById("nav_inner").classList.add('nav_inner_open');
         menu = 1;
     } else if (menu === 1) {
         console.log("closing menu");
         document.getElementById("nav").classList.remove('nav_open');
+        document.getElementById("nav_inner").classList.remove('nav_inner_open');
         menu = 0;
     }
 }
